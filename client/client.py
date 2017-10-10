356d87925680b08976f8c4935f521a1e1477ca72
thibault-lacroix <barret@lacroixrouge-brest.fr>
2017-10-10 11:55:57 +0200

    le serveur envoie les info gps

--- client/client.py
+++ client/client.py
@@ -3,28 +3,28 @@
 
 import socket
 
-hote="192.168.0.223"
+hote="127.0.0.1"
 port = 25500
 
 trame = bytearray([25,12,1,30])
-
+print "   "
 print "UDP target IP: ",hote
 print "UDP target port: ",port
 
 socket = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 socket.sendto(trame,(hote,port))
 print "message envoyé:", trame[0],trame[1],trame[2],trame[3]
+print "       "
+
 
 
 trameReponse,addr=socket.recvfrom(1024)
-#print "message recu: ",ord(data[0]),ord(data[1]),ord (data[2]),ord(data[3])
-#print "id: ",ord(data[0])
-#print "taille: ",ord(data[1])
-#print "safran: ",ord (data[2])
-#print "GV: ",ord(data[3])
-print "latitude: ",ord(trameReponse[0]),ord(trameReponse[1]),ord(trameReponse[2]),ord(trameReponse[3])
-
-
-
+print "Reponse du serveur: "
+print "         "
+print "latitude: ",hex(ord(trameReponse[3])),hex(ord(trameReponse[2])),",",hex(ord(trameReponse[1])),hex(ord(trameReponse[0]))
+print "longitude: ",ord(trameReponse[7]),ord(trameReponse[6]),ord(trameReponse[5]),ord(trameReponse[4])
+print "Gite: ",ord(trameReponse[8]),"°"
+print "Direction du vent: ",ord(trameReponse[9]),"°"
+print "Vitesse du vent: ",ord(trameReponse[10]),"nd"
 
 #socket.close
--- server/server_eth.py
+++ server/server_eth.py
@@ -3,7 +3,7 @@
 
 import socket
 
-hote='192.168.0.223'
+hote='127.0.0.1'
 port=25500
 
 socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -16,10 +16,19 @@ b3=(latitude>>24)&0xFF
 b2=(latitude>>16)&0xFF
 b1=(latitude>>8)&0xFF
 b0=latitude&0xFF
-print hex(b3)
-print hex(b2)
-print hex(b1)
-print hex(b0)
+
+longitude=2563015
+b7=(longitude>>24)&0xFF
+b6=(longitude>>16)&0xFF
+b5=(longitude>>8)&0xFF
+b4=longitude&0xFF
+
+g0=30 #gite
+
+dv=230 #direction du vent
+
+vv=10 #vitesse du vent
+
 
 while True:
     data,addr=socket.recvfrom(1024)
@@ -28,9 +37,8 @@ while True:
     print "taille: ",ord(data[1])
     print "safran: ",ord(data[2])
     print "GV: ",ord(data[3])
-    trameReponse=bytearray([b0,b1,b2,b3,data[1]])
+    trameReponse=bytearray([b0,b1,b2,b3,b4,b5,b6,b7,g0,dv,vv])
     socket.sendto(trameReponse,(addr))
-
 
 #print "fermeture"
 #client.close()
